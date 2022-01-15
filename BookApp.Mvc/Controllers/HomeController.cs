98e2a02a5cfccce5e943b49123feced948b1893e
Oğuzhan <oguzhanakpinar@yahoo.com>
2022-01-15 16:40:03 +0300

    service test

---
+++ BookApp.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using BookApp.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApp.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IGenreService _genreService;
+        private readonly IAuthorService _authorService;
+        private readonly IBookService _bookService;
+
+        public HomeController(IGenreService genreService, IAuthorService authorService, IBookService bookService)
+        {
+            _genreService = genreService;
+            _authorService = authorService;
+            _bookService = bookService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var books = await _bookService.GetAllAsync();
+            return View(books.Data);
+        }
+    }
+}
