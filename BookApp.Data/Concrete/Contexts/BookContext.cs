b9df4db41f72409afe564df9550d91aa7ea6136c
Oğuzhan <oguzhanakpinar@yahoo.com>
2022-01-15 14:11:10 +0300

    context eklendi

---
+++ BookApp.Data/Concrete/Contexts/BookContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookApp.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookApp.Data.Concrete.Contexts
+{
+    public class BookContext : IdentityDbContext<User, Role, int, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+    }
+}
