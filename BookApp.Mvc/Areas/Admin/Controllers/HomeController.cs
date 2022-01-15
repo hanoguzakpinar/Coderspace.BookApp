d2ef401ad76ba4bd6db35e72b1999f3056fec5c4
Oğuzhan <oguzhanakpinar@yahoo.com>
2022-01-15 17:01:05 +0300

    areas refactorin

---
+++ BookApp.Mvc/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApp.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookApp.Mvc/Areas/Member/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApp.Mvc.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class HomeController : Controller
+    {
+        [Authorize(Roles = "Member")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
