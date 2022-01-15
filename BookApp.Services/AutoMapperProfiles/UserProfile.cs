c122a16d6988f513e68b7313c98b1d5b8786aad1
Oğuzhan <oguzhanakpinar@yahoo.com>
2022-01-15 17:00:55 +0300

    auth views eklendi

---
+++ BookApp.Services/AutoMapperProfiles/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using BookApp.Entities;
+using BookApp.Entities.Dtos.AuthDtos;
+
+namespace BookApp.Services.AutoMapperProfiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserRegistrationDto>().ReverseMap();
+        }
+    }
+}
