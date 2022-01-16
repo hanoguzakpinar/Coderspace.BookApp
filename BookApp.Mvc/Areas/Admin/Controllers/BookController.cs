66f13ab4498a691c72115bdc671baa271bef3ef5
Oğuzhan <oguzhanakpinar@yahoo.com>
2022-01-16 12:40:36 +0300

    book-Index

---
+++ BookApp.Mvc/Areas/Admin/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using BookApp.Services.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApp.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class BookController : Controller
+    {
+        private readonly IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var books = await _bookService.GetAllAsync();
+            return View(books.Data);
+        }
+    }
+}
